c1482a9d3c6055fd9cd3c938febc319abfd1ce43
keerthi0396 <keerthanashan03@gmail.com>
2020-05-26 19:07:46 +0530

    updated

--- src/test/java/org/project/MyFirstProject.java
+++ src/test/java/org/project/MyFirstProject.java
@@ -3,8 +3,8 @@ package org.project;
 public class MyFirstProject {
 	public static void main(String[] args) {
 		System.out.println("hi dude");
-		
-		
+		System.out.println("xcv");
+		System.out.println("zxcv");
 	}
 
 }
